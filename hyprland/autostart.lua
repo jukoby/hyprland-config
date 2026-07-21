413e9567bf516d4700baebbfafa2a6b60389f790
jukoby <j.kobylanski@proton.me>
2026-07-21 23:28:13 +0200

    remove autostart entries and use systemd services instead

--- hyprland/autostart.lua
+++ hyprland/autostart.lua
@@ -3,9 +3,5 @@ local uwsm = require("hyprland.namespace.uwsm")
 hl.on("hyprland.start", function ()
     uwsm.exec_all({
         "hyprlock || hyprctl dispatch exit",
-        "walker --gapplication-service",
-        "quickshell",
-        "sunshine",
-        "$scripts/update-notifier.sh"
     })
 end)
