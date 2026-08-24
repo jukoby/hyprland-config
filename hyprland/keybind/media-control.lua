332720b3ca04b5f74a02f503eaac35b7a6edcbff
jukoby <j.kobylanski@proton.me>
2026-08-24 16:17:50 +0200

    enable media control when session is locked

--- hyprland/keybind/media-control.lua
+++ hyprland/keybind/media-control.lua
@@ -1,61 +1,85 @@
 local uwsm = require("hyprland.namespace.uwsm")
 
 hl.bind("XF86AudioRaiseVolume",
-    uwsm.dsp("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2.5%+"),
-    { desc = "Raise Volume" }
+    uwsm.dsp("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2.5%+"), {
+        desc = "Raise Volume",
+        locked = true
+    }
 )
 
 hl.bind("XF86AudioLowerVolume",
-    uwsm.dsp("wpctl set-volume @DEFAULT_AUDIO_SINK@ 2.5%-"),
-    { desc = "Lower Volume" }
+    uwsm.dsp("wpctl set-volume @DEFAULT_AUDIO_SINK@ 2.5%-"), {
+        desc = "Lower Volume",
+        locked = true
+    }
 )
 
 hl.bind("XF86AudioMute",
-    uwsm.dsp("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
-    { desc = "Toggle Mute" }
+    uwsm.dsp("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"), {
+        desc = "Toggle Mute",
+        locked = true
+    }
 )
 
 hl.bind("SHIFT + XF86AudioMute",
-    uwsm.dsp("pactl set-default-sink $(pactl list sinks short | awk '/SUSPENDED$/ {print $1}')"),
-    { desc = "" }
+    uwsm.dsp("pactl set-default-sink $(pactl list sinks short | awk '/SUSPENDED$/ {print $1}')"), {
+        desc = "",
+        locked = true
+    }
 )
 
 hl.bind("XF86AudioPlay",
-    uwsm.dsp("playerctl play-pause"),
-    { desc = "Play/Pause" }
+    uwsm.dsp("playerctl play-pause"), {
+        desc = "Play/Pause",
+        locked = true
+    }
 )
 
 hl.bind("SHIFT + XF86AudioPlay",
-    uwsm.dsp("playerctld shift || playerctld daemon"),
-    { desc = "" }
+    uwsm.dsp("playerctld shift || playerctld daemon"), {
+        desc = "",
+        locked = true
+    }
 )
 
 hl.bind("XF86AudioPrev",
-    uwsm.dsp("playerctl previous"),
-    { desc = "Previous Track" }
+    uwsm.dsp("playerctl previous"), {
+        desc = "Previous Track",
+        locked = true
+    }
 )
 
 hl.bind("SHIFT + XF86AudioPrev",
-    uwsm.dsp("playerctl position 15-"),
-    { desc = "Seek Back 15s" }
+    uwsm.dsp("playerctl position 15-"), {
+        desc = "Seek Back 15s",
+        locked = true
+    }
 )
 
 hl.bind("XF86AudioNext",
-    uwsm.dsp("playerctl next"),
-    { desc = "Next Track" }
+    uwsm.dsp("playerctl next"), {
+        desc = "Next Track",
+        locked = true
+    }
 )
 
 hl.bind("SHIFT + XF86AudioNext",
-    uwsm.dsp("playerctl position 15+"),
-    { desc = "Seek Forward 15s" }
+    uwsm.dsp("playerctl position 15+"), {
+        desc = "Seek Forward 15s",
+        locked = true
+    }
 )
 
 hl.bind("XF86MonBrightnessDown",
-    uwsm.dsp("playerctl volume 0.05-"),
-    { desc = "" }
+    uwsm.dsp("playerctl volume 0.05-"), {
+        desc = "",
+        locked = true
+    }
 )
 
 hl.bind("XF86MonBrightnessUp",
-    uwsm.dsp("playerctl volume 0.05+"),
-    { desc = "" }
+    uwsm.dsp("playerctl volume 0.05+"), {
+        desc = "",
+        locked = true
+    }
 )
